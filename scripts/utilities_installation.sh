a0ab4761c14197b53981a4d64e7ec858397bb0de
matteus <victormatteus@discente.ufg.br>
2023-11-24 20:44:03 -0300

    Add scripts folder and Update start.sh

---
+++ scripts/utilities_installation.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+
+#https://github.com/Tinram/Linux-Utilities.git
+
+sudo apt update
+
+sudo apt install -y \
+	nano \
+	git-all \
+	gh \
+	htop \
+	terminator \
+	gparted \
+	pip \
+	wget \
+	apt-utils \
+	net-tools \
+	vim \
+	nmon \
+	ncdu \
+	
---
+++ scripts/vscode_installation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+
+#Download the .deb file and go to its directory
+
+#sudo apt install ./<file>.deb
