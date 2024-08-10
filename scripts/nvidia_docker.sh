f14f1d30d6b01df293907b2164f5079030f58666
victormatteus04 <victormatteus@dicente.ufg.br>
2024-08-10 01:26:54 -0300

    added nvidia_docker_toolkit installation script

---
+++ scripts/nvidia_docker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Configure the production repository:
+
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
+sudo apt-get update
+
+
+# Install the NVIDIA Container Toolkit packages:
+
+sudo apt-get install -y nvidia-container-toolkit
+
+# Configure the container runtime by using the nvidia-ctk command:
+
+sudo nvidia-ctk runtime configure --runtime=docker
+
+sudo systemctl restart docker
